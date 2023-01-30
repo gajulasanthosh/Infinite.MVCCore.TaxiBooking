ecc9545d99d256fad970bd43e963a30f886069bf
SanthoshGajula <santhoshga@infinite.com>
2023-01-30 23:55:35 +0530

    update with Register Page

--- Infinite.MVCCore.TaxiBooking/Controllers/AccountsController.cs
+++ Infinite.MVCCore.TaxiBooking/Controllers/AccountsController.cs
@@ -1,8 +1,12 @@
 ﻿using Infinite.MVCCore.TaxiBooking.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Infinite.MVCCore.TaxiBooking.Controllers
@@ -35,7 +39,7 @@ namespace Infinite.MVCCore.TaxiBooking.Controllers
                     {
                         string token = await result.Content.ReadAsAsync<string>();
                         HttpContext.Session.SetString("token", token);
-                        return RedirectToAction("Index", "Bookings");
+                        return RedirectToAction("Index", "Customers");
 
                     }
                     ModelState.AddModelError("", "Invalid LoginID or Password");
@@ -43,7 +47,73 @@ namespace Infinite.MVCCore.TaxiBooking.Controllers
             }
             return View(login);
         }
+
+        [HttpPost]
+        public IActionResult LogOut()
+        {
+            HttpContext.Session.Remove("token");
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RolesRegisterVM model = new RolesRegisterVM
+            {
+                Values = new List<SelectListItem>
+                    {
+                        new SelectListItem {Value = "Employee", Text="Employee"},
+                        new SelectListItem{Value="Customer",Text="Customer"}
+                    }
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register([FromForm] RolesRegisterVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Clear(); client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
+                    client.BaseAddress = new Uri(_configuration["ApiUrl:api"]);
+                    RegisterViewModel user = new RegisterViewModel
+                    {
+                        LoginID = model.RegisterRoles.LoginID,
+                        Password = model.RegisterRoles.Password,
+                        ConfirmPassword = model.RegisterRoles.ConfirmPassword,
+                        Role = model.SelectedValue
+                    };
+
+                    var result = await client.PostAsJsonAsync("Accounts/Register", user);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Login");
+                    }
+                }
+            }
+            RegisterViewModel user1 = new RegisterViewModel
+            {
+                LoginID = model.RegisterRoles.LoginID,
+                Password = model.RegisterRoles.Password,
+                Role = model.SelectedValue
+            };
+            RolesRegisterVM model1 = new RolesRegisterVM
+            {
+                RegisterRoles = user1,
+                Values = new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "Customer", Text = "Customer" },
+                    new SelectListItem { Value = "Employee", Text = "Employee" },
+                }
+            };
+            return View(model1);
+        }
+
     }
+
+
 }
 
 
